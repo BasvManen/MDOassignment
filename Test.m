20348ace96968e156f4a123147cb800df965d2ea
Bas <BasvManen@Gmail.com>
2019-10-16 21:28:13 +0200

    Functie verbeterd met nieuwe design vector

--- Test.m
+++ Test.m
@@ -1,5 +1,43 @@
+%% Aerodynamic solver setting
 clear all
-x = [13.32 11.18 0.57 0.59 4.7 34 0 0];
+close all
+clc
 
-geom = calcwing(x);
-plot([geom(1,1) geom(2,1) geom(3,1) geom(3,1)+geom(3,4) geom(2,1)+geom(2,4) geom(1,1)+geom(1,4)],[geom(1,2) geom(2,2) geom(3,2) geom(3,2) geom(2,2) geom(1,2)]);
+x = [17.64 11.96 2.6312 35 0 0];
+% Wing planform geometry 
+%                x    y     z   chord(m)    twist angle (deg) 
+AC.Wing.Geom = calcwing(x);
+
+% Wing incidence angle (degree)
+AC.Wing.inc  = 0;   
+            
+            
+% Airfoil coefficients input matrix
+%                    | ->     upper curve coeff.                <-|   | ->       lower curve coeff.       <-| 
+AC.Wing.Airfoils   = [0.2171    0.3450    0.2975    0.2685    0.2893  -0.1299   -0.2388   -0.1635   -0.0476    0.0797;
+                      0.2171    0.3450    0.2975    0.2685    0.2893  -0.1299   -0.2388   -0.1635   -0.0476    0.0797];
+                  
+AC.Wing.eta = [0;1];  % Spanwise location of the airfoil sections
+
+% Viscous vs inviscid
+AC.Visc  = 0;              % 0 for inviscid and 1 for viscous analysis
+AC.Aero.MaxIterIndex = 150;    %Maximum number of Iteration for the
+                                %convergence of viscous calculation
+                                
+                                
+% Flight Condition
+AC.Aero.V     = 68;            % flight speed (m/s)
+AC.Aero.rho   = 1.225;         % air density  (kg/m3)
+AC.Aero.alt   = 0;             % flight altitude (m)
+AC.Aero.Re    = 1.14e7;        % reynolds number (bqased on mean aerodynamic chord)
+AC.Aero.M     = 0.2;           % flight Mach number 
+% AC.Aero.CL    = 0.4;          % lift coefficient - comment this line to run the code for given alpha%
+AC.Aero.Alpha = 0;             % angle of attack -  comment this line to run the code for given cl 
+
+
+%% 
+tic
+
+Res = Q3D_solver(AC);
+
+toc
--- calcwing.m
+++ calcwing.m
@@ -1,13 +1,11 @@
 function geom = calcwing(x)
 
-S_1 = x(1);
-S_2 = x(2);
-taper_1 = x(3);
-taper_2 = x(4);
-c_r = x(5);
-sweep_LE = pi*x(6)/180;
-e_t1 = x(7);
-e_t2 = x(8);
+b = x(1);
+c_r = x(2);
+c_t = x(3);
+sweep_LE = pi*x(4)/180;
+e_t1 = x(5);
+e_t2 = x(6);
 
 %Chord
 geom(1,1) = 0;
@@ -17,19 +15,17 @@ geom(1,4) = c_r;
 geom(1,5) = 0;
 
 %Kink
-b_1 = ((2*S_1)/((1+taper_1)*c_r));
-geom(2,1) = b_1*tan(sweep_LE);
-geom(2,2) = b_1;
+geom(2,1) = 7.56*tan(sweep_LE);
+geom(2,2) = 7.56;
 geom(2,3) = 0;
-geom(2,4) = taper_1*c_r;
+geom(2,4) = c_r+7.56*(tan(pi*6/180)-tan(sweep_LE));
 geom(2,5) = e_t1;
 
 %Tip
-b_2 = b_1+((2*S_2)/((taper_1+taper_1*taper_2)*c_r));
-geom(3,1) = b_2*tan(sweep_LE);
-geom(3,2) = b_2;
+geom(3,1) = (7.56+b)*tan(sweep_LE);
+geom(3,2) = 7.56+b;
 geom(3,3) = 0;
-geom(3,4) = taper_1*taper_2*c_r;
+geom(3,4) = c_t;
 geom(3,5) = e_t2;
 end
 
